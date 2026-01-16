8725e1ffda12e3cadf94c4562396d9e7461d1c6a
haomaamoah <77377960+haomaamoah@users.noreply.github.com>
2026-01-16 18:49:57 +0000

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+
+import '../providers/homework.dart';
+import '../providers/identifyeverything.dart';
+import '../views/splashscreen/splash.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  // Initialize environment variables before running the app
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => HomeworkSolutionProvider()),
+        ChangeNotifierProvider(create: (_) => IdentifyEverythingProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: SplashScreen(),
+    );
+  }
+}
